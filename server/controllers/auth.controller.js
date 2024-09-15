e19bd5983d59a71859ef2110f13f0d075a47b79a
Mohammed <dev.mohammedsh@gmail.com>
2024-09-15 17:31:30 +0530

    🌱 Server setup

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+	res.send("Hello world")
+}
+export const login = async (req, res) => {
+	res.send("Hello world")
+}
+export const logout = async (req, res) => {
+	res.send("Hello world")
+}
