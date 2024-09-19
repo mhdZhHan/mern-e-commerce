6d770fb26b9a42af7eec13d7def8887d6f0fdfbc
Mohammed <dev.mohammedsh@gmail.com>
2024-09-19 19:24:34 +0530

    👍 Done cart controller

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,5 +1,20 @@
+import Product from "../models/product.model.js"
+
 export const getCartProducts = async (req, res) => {
 	try {
+		const products = await Product.find({
+			_id: { $in: req.user.cartItems },
+		})
+
+		// add quantity fro each product
+		const cartItems = products.map((product) => {
+			const item = req.user.cartItems.find(
+				(cartItem) => cartItem.id === product.id
+			)
+			return { ...product.toJSON(), quantity: item.quantity }
+		})
+
+		res.status(200).json(cartItems)
 	} catch (error) {
 		console.log("Error in getCartProducts controller", error?.message)
 		res.status(500).json({ message: "Server error", error: error?.message })
@@ -8,6 +23,22 @@ export const getCartProducts = async (req, res) => {
 
 export const addToCart = async (req, res) => {
 	try {
+		const { productId } = req.body
+		const user = req.user
+
+		const existingItem = user.cartItems.find(
+			(item) => item.id === productId
+		)
+
+		if (existingItem) {
+			existingItem.quantity += 1
+		} else {
+			user.cartItems.push(productId)
+		}
+
+		await user.save()
+
+		res.status(200).json(user.cartItems)
 	} catch (error) {
 		console.log("Error in addToCart controller", error?.message)
 		res.status(500).json({ message: "Server error", error: error?.message })
@@ -16,6 +47,20 @@ export const addToCart = async (req, res) => {
 
 export const removeAllFromCart = async (req, res) => {
 	try {
+		const { productId } = req.body
+
+		const user = req.user
+
+		if (!productId) {
+			user.cartItems = []
+		} else {
+			user.cartItems = user.cartItems.filter(
+				(item) => item.id !== productId
+			)
+		}
+
+		await user.save()
+		res.status(200).json(user.cartItems)
 	} catch (error) {
 		console.log("Error in removeAllFromCart controller", error?.message)
 		res.status(500).json({ message: "Server error", error: error?.message })
@@ -24,6 +69,32 @@ export const removeAllFromCart = async (req, res) => {
 
 export const updateQuantity = async (req, res) => {
 	try {
+		const { id: productId } = req.params
+		const { quantity } = req.body
+
+		const user = req.user
+
+		const existingItem = user.cartItems.find(
+			(item) => item.id === productId
+		)
+
+		if (existingItem) {
+			if (quantity === 0) {
+				user.cartItems = user.cartItems.filter(
+					(item) => item.id !== productId
+				)
+
+				await user.save()
+				return res.status(200).json(user.cartItems)
+			}
+
+			existingItem.quantity = quantity
+			await user.save()
+
+			res.status(200).json(user.cartItems)
+		} else {
+			res.status(404).json({ message: "Product not found" })
+		}
 	} catch (error) {
 		console.log("Error in updateQuantity controller", error?.message)
 		res.status(500).json({ message: "Server error", error: error?.message })
