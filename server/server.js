3f6f6ba013a86ffcedbd723ac1c41cc6cd53c846
Mohammed <dev.mohammedsh@gmail.com>
2024-09-19 21:26:09 +0530

    ✏️ Typo && 💲 payments route start dev

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ import { connectDb } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
-import couponsRoutes from "./routes/coupon.route.js"
+import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 
 dotenv.config()
 
@@ -21,7 +22,8 @@ app.use(cookieParser()) // allow parse incoming cookies
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
-app.use("/api/coupons", couponsRoutes)
+app.use("/api/coupons", couponRoutes)
+app.use("/api/payments", paymentRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
