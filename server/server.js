39ced1db2e5cf675e053fb0f532bbafbac1197a8
Mohammed <dev.mohammedsh@gmail.com>
2024-10-03 06:57:51 +0530

    🚢 Configured for deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "node:path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -16,6 +17,8 @@ dotenv.config()
 
 const app = express()
 
+const __dirname = path.resolve()
+
 // middlewares
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json({ limit: "10mb" }))
@@ -28,6 +31,14 @@ app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(__dirname, "/client/dist"))
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+	})
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
