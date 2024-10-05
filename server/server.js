a1ef4a67ff76247e03bae5396c324d76e73342d4
Mohammed <dev.mohammedsh@gmail.com>
2024-10-05 09:09:56 +0530

    🐞 Fix for production

--- server/server.js
+++ server/server.js
@@ -33,13 +33,12 @@ app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(__dirname, "/client/dist"))
+	app.use(express.static(path.join(__dirname, "/client/dist")))
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
 	})
 }
-
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`)
